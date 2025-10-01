742068a4d287c3da6fbe07522c5b8af212c97df5
Abderrafie Chate | الشاط عبدالرفيع <chattabdrrafie@gmail.com>
2025-10-01 14:26:06 +0100

    Phone Notifications

--- LANGUAGES/KOTLIN/02 - PRACTICES/PRACTICE N - 001.kt
+++ LANGUAGES/KOTLIN/02 - PRACTICES/PRACTICE N - 001.kt
@@ -1,1 +1,22 @@
-// test
+// Phone Notifications - practice: 
+
+// The exact number of notifications when there are less than 100 notifications.
+// 99+ as the number of notifications when there are 100 notifications or more.
+
+
+fun main() {
+    val morningNotification = 51
+    val eveningNotification = 135
+    
+    printNotificationSummary(morningNotification)
+    printNotificationSummary(eveningNotification)
+}
+
+
+fun printNotificationSummary(numberOfMessages: Int) {
+    if (numberOfMessages < 100) {
+        println("You Have $numberOfMessages Message !")
+    } else if (numberOfMessages >= 100) {
+        println("Your phone is blowing up! You have 99+ notifications.")
+    }
+}
