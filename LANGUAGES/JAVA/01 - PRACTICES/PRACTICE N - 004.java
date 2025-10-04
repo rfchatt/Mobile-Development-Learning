38bda367471f8ed3727a3d9fed7b67e962e5105d
Abderrafie Chate | الشاط عبدالرفيع <chattabdrrafie@gmail.com>
2025-10-04 20:06:09 +0100

    TP 4

--- LANGUAGES/JAVA/01 - PRACTICES/PRACTICE N - 004.java
+++ LANGUAGES/JAVA/01 - PRACTICES/PRACTICE N - 004.java
@@ -1,36 +1,30 @@
 import java.util.Scanner;
+
 // TP 4
 // Exercise 1
 
-class remplissage {
+class main{
     public static void main(String[] args) {
-        Scanner Objet4 = new Scanner(System.in);
+        Scanner Objet = new Scanner(System.in);
 
-        int[][] table = new int[2][3];
+        int[][] tableau2D = new int[2][3];
 
-        System.out.println("[horizontal] Entrez un nombre : ");
-        int horizontal1 = Objet4.nextInt();
-        System.out.println("[vertical] Entrez un nombre : ");
-        int vertical1 = Objet4.nextInt();
-        System.out.println("[vertical] Entrez un nombre : ");
-        int vertical2 = Objet4.nextInt();
-        System.out.println("[vertical] Entrez un nombre : ");
-        int vertical3 = Objet4.nextInt();
+        System.out.println("Veuillez remplir ce tableau : \n");
 
-        System.out.println("[horizontal] Entrez un nombre : ");
-        int horizontal2 = Objet4.nextInt();
-        System.out.println("[vertical] Entrez un nombre : ");
-        int vertical1_2 = Objet4.nextInt();
-        System.out.println("[vertical] Entrez un nombre : ");
-        int vertical2_2 = Objet4.nextInt();
-        System.out.println("[vertical] Entrez un nombre : ");
-        int vertical3_2 = Objet4.nextInt();
-
-
-        System.out.println("------------");
-        System.out.println("| p1  |  p2 |");
-        System.out.println("| " + horizontal1 + " | " + horizontal2 + " | ");
-        System.out.println("| " + vertical1 + " | " + vertical1_2 + " | " + "\n" + "| " + vertical2 + " | " + vertical2_2 + " | " +"\n" + "| " + vertical3 + " | " + vertical3_2 + " | ");
-        System.out.println("------------");
+        for (int i = 0; i < tableau2D.length; i++) {
+            for (int j = 0; j < tableau2D[i].length; j++) {
+                System.out.println("entrer la valeur de ligne [" + i + "] colonne[" + j + "] : ");
+                tableau2D[i][j] = Objet.nextInt();
+            }
+        }
+        System.out.println("_________________\n");
+        for (int i = 0; i < tableau2D.length; i++) {
+            System.out.println("\n");
+            for (int j = 0; j < tableau2D[i].length; j++) {
+                System.out.print("| " + tableau2D[i][j] + " ");
+            }
+            System.out.print("|");
+        }
+        System.out.println("\n___________________");
     }
 }
