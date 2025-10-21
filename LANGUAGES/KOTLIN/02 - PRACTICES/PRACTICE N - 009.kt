896386c910600cdc10357fe01fdff7e9477a6f0f
Abderrafie Chate | الشاط عبدالرفيع <chattabdrrafie@gmail.com>
2025-10-21 13:08:13 +0100

    TP 5 - OOp

--- LANGUAGES/KOTLIN/02 - PRACTICES/PRACTICE N - 009.kt
+++ LANGUAGES/KOTLIN/02 - PRACTICES/PRACTICE N - 009.kt
@@ -1,5 +1,147 @@
 package TP4_KOTLIN
 
+// Exercise 1 ---
+
+// class abstraite Form
+abstract class Forme (var couleur: String) {
+
+    abstract fun afficherInfo ();
+
+}
+
+// sous-class Cercle
+class Cercle (couleur: String, var rayon: Int) : Forme (couleur = couleur) {
+
+    override fun afficherInfo() {
+        println("Le couleur de cercle : $couleur, et le Rayon : $rayon")
+    }
+}
+
+// sous-class Rectangle
+class Rectangle (couleur: String, var largeur: Int, var hauteur: Int) : Forme (couleur = couleur) {
+
+
+    override fun afficherInfo() {
+         println("Le couleur de Rectangle : $couleur, largeur : $largeur et l'hauteur : $hauteur")
+    }
+}
+
+
+// l'Execution
+fun main () {
+
+    var c = Cercle("Bleu", 5)
+    c.afficherInfo()
+    println()
+
+    var r = Rectangle("Orange", 30, 25)
+    r.afficherInfo()
+
+}
+
+// ----------------------------------------------------------------------
+
+package TP4_KOTLIN
+
+// Exercise 2 ---
+
+abstract class Personne (var nom: String, var age: Int) {
+
+    abstract fun afficherDetails ();
+
+}
+
+class Eleve (nom: String, age: Int, var niveau: String) : Personne (nom = nom, age = age) {
+
+    override fun afficherDetails() {
+         println("Le nom d'éléve : $nom, l'áge : $age et le niveau : $niveau")
+    }
+
+}
+
+class Employe (nom: String, age: Int, var poste: String) : Personne (nom = nom, age = age) {
+
+    override fun afficherDetails() {
+         println("Le nom de l'emploiyé : $nom, l'áge : $age, et le poste : $poste")
+    }
+
+}
+
+
+// l'Execution
+fun main () {
+
+    println()
+    var e1 = Eleve("Abderrafie", 20, "TS")
+    var e2 = Eleve("Anass", 18, "Bac")
+    e1.afficherDetails()
+    e2.afficherDetails()
+
+    println()
+    var em1 = Employe("Oussama", 27, "Ingénieur")
+    var em2 = Employe("Yassin", 25, "Technicien électricité")
+    em1.afficherDetails()
+    em2.afficherDetails()
+
+}
+
+// ----------------------------------------------------------------------
+
+package TP4_KOTLIN
+
+// Exercise 3 ---
+
+abstract class Vehicule (var marque: String) {
+
+    init {
+        println("Création de l'objet '$marque' effectué avec succès !")
+    }
+
+    abstract fun afficherInfo ();
+
+}
+
+class Voiture (marque: String, var nombrePortes: Int) : Vehicule (marque = marque) {
+
+    override fun afficherInfo() {
+        println("La marque de Voiture : $marque, et le nombre des portes : $nombrePortes")
+    }
+
+}
+
+class Camion (marque: String, var capaciteCharge: Int) : Vehicule (marque = marque) {
+
+    override fun afficherInfo() {
+        println("La marque de Camion : $marque, et la capacite de charge : $capaciteCharge Kg")
+    }
+
+}
+
+
+// L'execution
+fun main () {
+
+    println()
+    var v1 = Voiture("Mercedes", 5)
+    var v2 = Voiture("Renault", 6)
+    v1.afficherInfo()
+    v2.afficherInfo()
+
+    println()
+    var c1 = Camion("Volvo", 1000)
+    var c2 = Camion("Mercedes", 1200)
+    c1.afficherInfo()
+    c2.afficherInfo()
+
+}
+
+// ----------------------------------------------------------------------
+
+
+package TP4_KOTLIN
+
+// Exercise 4 ---
+
 abstract class Appareil (var marque: String, var allume: Boolean = false) {
 
     abstract val typeAppareil: String
