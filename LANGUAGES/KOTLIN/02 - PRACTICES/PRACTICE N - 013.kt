9d31fee49395ae5b2297e0589b744eec10ab6213
Abderrafie Chate | الشاط عبدالرفيع <chattabdrrafie@gmail.com>
2025-10-28 12:06:34 +0100

    TP 6 - OOP Global v2

--- LANGUAGES/KOTLIN/02 - PRACTICES/PRACTICE N - 013.kt
+++ LANGUAGES/KOTLIN/02 - PRACTICES/PRACTICE N - 013.kt
@@ -2,12 +2,8 @@ package TP6_KT_GLOBAL_2
 
 import kotlin.collections.List
 
-// TP 6 - KOTLIN GLOBAL v2
-
 // 1- Class Spectateur
-class Spectateur (var nom: String, var age: Int, var email: String) {
-
-}
+class Spectateur (var nom: String, var age: Int, var email: String)
 
 // 2- Class Film
 open class Film (var titre: String, var duree: Int, nom: String, description: String) : Genre (nom, description) {
@@ -42,9 +38,8 @@ abstract class FilmAction (titre: String, duree: Int, var niveauViolance: Int, n
 }
 
 // 5- Class Billet
-abstract class Billet (var numeroBillet: String) {
+abstract class Billet (var numeroBillet: String)
 
-}
 class BilletVIP (numeroBillet: String) : Billet (numeroBillet) {
 
     var avantages = mutableListOf<String>()
@@ -52,9 +47,7 @@ class BilletVIP (numeroBillet: String) : Billet (numeroBillet) {
 }
 
 // 6- Class Genre
-open class Genre (var nom: String, var description: String) {
-
-}
+open class Genre (var nom: String, var description: String)
 
 // 7- Class Evaluation
 class Evaluation (var note: Int, var commentaire: String) {
@@ -62,6 +55,7 @@ class Evaluation (var note: Int, var commentaire: String) {
     fun ajouterEvaluation ( listeEvaluations: MutableList<Double>, note: Int) {
         listeEvaluations.add(note.toDouble())
     }
+
     fun calculerMoyenne (listeEvaluations: MutableList<Double>): Double {
         return listeEvaluations.average()
     }
@@ -69,19 +63,34 @@ class Evaluation (var note: Int, var commentaire: String) {
 }
 
 // 8- Extension Functions
-fun MutableList<Film>.filtrerDuree (duree: Int): List<Film> {
-    return this.filter { it.duree == duree }
+fun MutableList<Film>.filtrerParDuree (duree: Int): List<Film> {
+    return this.filter { it.duree >= duree }
 }
+
 fun Film.nbrTotalSpects (spectateur: Spectateur): Int {
     return this.spectateurs.size
 }
 
-// Class Seance
+// 9- Class Seance
 class Seance (var horaire: String, var salle: Int, var film: Film) {
 
+    var placesDisponible: Int = 200
+
     fun gererPlacesDispos () {
+        println("Les places disponibles : $placesDisponible")
+    }
+
+    fun reserverPlace () {
+        if (placesDisponible > 0) {
+            placesDisponible --
+            println("Place réservée ! Places restantes : $placesDisponible")
+        } else {
+            println("Toutes les tickets sont vendus !")
+        }
     }
 
 }
 
-// ... A suivre
+// 10- Programme Principal : 
+
+// A Suivre ... inshaAllah !
