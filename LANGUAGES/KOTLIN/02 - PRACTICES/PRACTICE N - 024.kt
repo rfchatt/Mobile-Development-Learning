dee42b60f1e94f7f426ef87fa39b5fdfd38fec93
Abderrafie Chate | الشاط عبدالرفيع <chattabdrrafie@gmail.com>
2025-11-17 16:34:18 +0100

    TP 16 ( lateinit / by lazy / Try-Catch )

---
+++ LANGUAGES/KOTLIN/02 - PRACTICES/PRACTICE N - 024.kt
@@ -0,0 +1,145 @@
+package TP16_KOTLIN
+
+// TP 16 | Lateinit, By lazy, Try-catch
+
+// Exercice 1 : --
+
+class Vehicule () {
+
+    lateinit var name: String
+
+    val description: String by lazy {
+        println("Description ...")
+        "Nom : $name"
+    }
+
+}
+
+
+
+fun main() {
+
+    var obj = Vehicule()
+
+    println("--- Avant l'Initialisation ---")
+    // println(obj.description)     // Erreur
+
+    println("-- l'Initialisation --")
+    obj.name = "Dacia"
+
+    println("-- Aprés Initialisation --")
+    println(obj.description)
+
+}
+
+// Run : 
+
+//--- Avant l'Initialisation ---
+//-- l'Initialisation --
+//-- Aprés Initialisation --
+//Description ...
+//Nom : Dacia
+
+
+// -----------------------------------------------------------------------
+
+
+package TP16_KOTLIN
+
+// Exercice 2 : --
+
+var num1 = 37
+var num2 = 19
+
+val addition by lazy {
+    num1 + num2
+}
+
+val soustraction by lazy {
+    num1 - num2
+}
+
+val multiplication by lazy {
+    num1 * num2
+}
+
+val division by lazy {
+    num1 / num2
+}
+
+fun main() {
+
+    println("Addition de [$num1] (+) [$num2] = $addition")
+    println("Soustraction [$num1] (-) [$num2] = $soustraction")
+    println("Multiplication [$num1] (x) [$num2] = $multiplication")
+    println("Division [$num1] (/) [$num2] = $division")
+
+}
+
+// Run :
+
+//Addition de [37] (+) [19] = 56
+//Soustraction [37] (-) [19] = 18
+//Multiplication [37] (x) [19] = 703
+//Division [37] (/) [19] = 1
+
+
+// -----------------------------------------------------------------------
+
+
+package TP16_KOTLIN
+
+// Exercice 3 : --
+
+open class Configuration () {
+
+    init {
+        println("Configuration en cours..")
+        Thread.sleep(2000)
+    }
+
+    var databasURL = "mysql://localhost:3306/rfchatt"
+
+    fun afficherConf () {
+        println("Configuration compléte : ")
+        println(" - Base de données : $databasURL")
+    }
+
+}
+
+class App {
+
+    val config by lazy {
+        println("Début de chargement..")
+        Configuration()
+    }
+
+    fun start() {
+        println("Application démarée !")
+        println("Configuration pas encours chargée..")
+    }
+
+}
+
+fun main () {
+
+    var app = App()
+
+    app.start()
+
+    app.config.afficherConf()
+
+}
+
+// Run :
+
+// Application démarée !
+// Configuration pas encours chargée..
+// Début de chargement..
+// Configuration en cours..
+// Configuration compléte :
+// - Base de données : mysql://localhost:3306/rfchatt
+
+
+// -----------------------------------------------------------------------
+
