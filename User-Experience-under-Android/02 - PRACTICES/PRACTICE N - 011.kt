ceed13f2d9d3b6592a4f9e0aa937857a6b2e26b5
Abderrafie Chate | الشاط عبدالرفيع <chattabdrrafie@gmail.com>
2026-01-21 15:41:27 +0100

    TP 12 - EX 2 (Hamcrest)

---
+++ User-Experience-under-Android/02 - PRACTICES/PRACTICE N - 011.kt
@@ -0,0 +1,36 @@
+// TP 12 EX - 2 (Hamcrest)
+
+// -------- | validerMotDePasse | --------
+
+package com.example.test_tps
+
+fun validerMotDePasse (mdp: String): Boolean {
+
+    val _8length = mdp.length >= 8
+    val _1mauscule = mdp.any { it.isUpperCase() }
+    val _1digit = mdp.any { it.isDigit() }
+
+    return _8length && _1mauscule && _1digit
+
+}
+
+// -------- | PasswordTest | --------
+
+package com.example.test_tps
+
+import kotlin.test.Test
+import kotlin.test.*
+
+class PasswordTest {
+
+    @Test
+    fun testValidPassword() {
+        assertThat(validerMotDePasse("MonCode123"), `is`(True)) // <-
+    }
+
+    @Test
+    fun testInvalidPasswords() {
+        assertThat("Trop court", validerMotDePasse("abc1234"), `is`(false)) // <-
+    }
+
+}
